f28b35820428fa60cd0749a7122efd063548e15e
Globusin <karpov.globik@yandex.ru>
2026-03-14 18:40:39 +0300

    Изменены уровни логирования в проекте

--- utils/hotspot.py
+++ utils/hotspot.py
@@ -3,7 +3,7 @@ import re
 import sys
 import logging
 
-from utils.logger import log_info, log_warning, log_error
+from utils.logger import log_info, log_warning, log_error, log_debug
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -17,20 +17,20 @@ def start_hotspot(ssid, password):
         password (str): Пароль для подключения к сети
     """
     try:
-        log_info(f"Попытка создания Wi-Fi хотспота с SSID: {ssid}")
+        log_debug(f"Попытка создания Wi-Fi хотспота с SSID: {ssid}")
         
         # Останавливаем возможную предыдущую точку доступа
-        log_info("Остановка предыдущей точки доступа (если существует)")
+        log_debug("Остановка предыдущей точки доступа (если существует)")
         result = subprocess.run(["nmcli", "connection", "down", "Hotspot"], check=False, capture_output=True, text=True)
         if result.returncode != 0:
             log_warning(f"Не удалось отключить предыдущую точку доступа: {result.stderr}")
         
-        log_info("Получение интерфейса")
+        log_debug("Получение интерфейса")
         ifname = get_wifi_interface_name()
         log_info("Получен интерфейс: " + ifname)
 
         # Запускаем точку доступа
-        log_info("Запуск точки доступа")
+        log_debug("Запуск точки доступа")
         subprocess.run(["rfkill", "unblock", "wifi"], check=True)
         subprocess.run(["nmcli", "radio", "wifi", "on"], check=True)
 
@@ -44,11 +44,9 @@ def start_hotspot(ssid, password):
             "password", password
         ]
 
-        log_info("Создание новой точки доступа")
+        log_debug("Создание новой точки доступа")
         result = subprocess.run(cmd, check=True, capture_output=True, text=True)
         log_info("Точка доступа создана успешно")
-
-        log_info(f"Wi-Fi хотспот '{ssid}' успешно запущен")
 
     except subprocess.CalledProcessError as e:
         log_error(f"Ошибка при создании Wi-Fi хотспота: {e}")
